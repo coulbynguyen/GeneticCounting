cf402b2acdf21817773fc328e8bc2d9a802a27d7
Nguyen <nguyenco@oregonstate.edu>
2020-02-17 13:37:46 -0800

    added initial files

---
+++ gambler.h
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+#ifndef GAMBLER
+#define GAMBLER
+
+class gambler{
+  public:
+    int mycounts;
+    int bet_amount;
+    int bet_choice;
+    int moneyleft;
+    int total_money_over_10_shoes;
+
+
+};
+
+#endif
